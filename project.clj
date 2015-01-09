dbdc61e024875f29aa370ce6c03075af4bea47f8
Stephen Brady <stephen.brady@gmail.com>
2015-01-09 02:02:30 -0800

    Add clojure 1.5.0 as a dependency. Use of cond-> requires it.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://www.github.com/Yuppiechef/datomic-schema"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
